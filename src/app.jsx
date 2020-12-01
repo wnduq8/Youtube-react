dc0f43408efb14fdfbf14d749b045436566b5d5f
juYeop <wnduq8@gmail.com>
2020-12-01 18:16:02 +0900

    list 선택에 따른 ui 변경 및 검색 후 레이아웃 변경

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from "react";
-import styles from "./app.module.css";
 import SearchHeader from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
+import styles from "./app.module.css";
+import VideoDetail from "./components/video_detail/video_detail";
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
 
   const onSerach = (value) => {
+    setSelectedVideo(null);
     youtube
       .search(value) //
       .then((videos) => setVideos(videos));
@@ -21,7 +28,20 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearchHeader onSerach={onSerach} />
-      <VideoList videos={videos} />;
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
     </div>
   );
 }
